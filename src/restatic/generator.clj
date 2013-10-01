1ef5e1d9167af87e52697be9ed48f80daf323d23
Filippo Diotalevi <filippo.diotalevi@gmail.com>
2013-10-01 09:16:43 +0100

    updated readme, removed the boilerplate

--- src/restatic/generator.clj
+++ src/restatic/generator.clj
@@ -2,7 +2,6 @@
   (:require [restatic.config :as config]
             [clostache.parser :as renderer]))
 
-
 (defn generate-index 
   [basedir output-dir]
   (spit (str basedir "/"  output-dir "/index.html") (renderer/render (slurp (str basedir "/views/" "index.mustache")) {})))
